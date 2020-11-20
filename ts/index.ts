124ec29365b76dadd39ceed5efc0c8c49e854783
Mathieu PATUREL <australie.p@gmail.com>
2020-11-20 21:03:38 +1100

    plot: use class instead of messy function

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.querySelector("#canvas");
+  const canvas = document.querySelector<HTMLCanvasElement>("#canvas");
   if (!canvas) throw new Error("no canvas");
   const origin = { x: -0.6, y: 0 };
   const scale = { x: 32, y: 32 };
-  initiatePlot(
-    canvas as HTMLCanvasElement,
-    origin,
-    scale,
-    (x) => (x * x * x) / 50
-  );
+
+  const plot = new Plot(canvas, origin, scale);
+  plot.func = (x) => (x * x * x) / 30;
+  plot.render();
 });
--- ts/plot.ts
+++ ts/plot.ts
@@ -8,143 +8,126 @@ interface MPoint {
   y: number;
 }
 
-function initiatePlot(
-  canvas: HTMLCanvasElement,
-  unitaryorigin: Point,
-  scale: Point,
-  f: (x: number) => number
-) {
-  const c = canvas.getContext("2d");
-  if (!c) {
-    throw new Error("no context");
-  }
+const Zero = { x: 0, y: 0 };
 
-  const origin: MPoint = {
-    x: (unitaryorigin.x * canvas.width) / 2 + canvas.width / 2,
-    y: (unitaryorigin.y * canvas.height) / 2 + canvas.height / 2,
-  };
+class Plot {
+  canvas: HTMLCanvasElement;
+  scale: Point;
+  origin: MPoint;
+  context: CanvasRenderingContext2D;
+  func: ((x: number) => number) | null;
+  pressdown: Point | null;
 
-  if (scale.x < 0 || scale.y < 0) {
-    throw new Error("negative scale");
-  }
+  constructor(canvas: HTMLCanvasElement, unitaryOrigin: Point, scale: Point) {
+    this.func = null;
+    this.pressdown = null;
 
-  const render = (origin: Point, scale: Point) => {
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    renderAxis(canvas, c, origin, scale);
+    this.canvas = canvas;
+    const context = this.canvas.getContext("2d");
+    if (context === null) throw new Error("null context for canvas");
+    this.context = context;
 
-    const left = Math.round((0 - origin.x) / scale.x);
-    const right = Math.round((canvas.width - origin.x) / scale.x);
+    this.scale = scale;
+    if (this.scale.x <= 0 || this.scale.y <= 0)
+      throw new Error("null or negative scale");
 
-    const RADIUS = 4;
-    c.fillStyle = "#2c3e50";
-    for (let x = left; x < right; x++) {
-      c.beginPath();
-      const y = f(x);
-      const p = project(origin, scale, { x, y });
-      c.ellipse(p.x, p.y, RADIUS, RADIUS, 0, 0, 2 * Math.PI);
-      c.fill();
-    }
-  };
-
-  registerOnMouseDrag(
-    canvas,
-    (shift: Point) => {
-      render(
-        {
-          x: origin.x + shift.x,
-          y: origin.y + shift.y,
-        },
-        scale
-      );
-    },
-    (shift: Point) => {
-      origin.x += shift.x;
-      origin.y += shift.y;
-      render(origin, scale);
-    }
-  );
-
-  render(origin, scale);
-}
-
-function assertIntegers(...nums: number[]) {
-  for (let num of nums) {
-    if (Math.floor(num) != num) {
-      throw new Error("not integer");
-    }
-  }
-}
-
-// flip vertically, then scales, then translates to origin, in this order
-function project(origin: Point, scale: Point, p: Point): Point {
-  // maths does the opposite of what programers do
-  return {
-    x: p.x * scale.x + origin.x,
-    y: -p.y * scale.y + origin.y,
-  };
-}
-
-function renderAxis(
-  canvas: HTMLCanvasElement,
-  c: CanvasRenderingContext2D,
-  origin: Point,
-  scale: Point
-) {
-  c.beginPath();
-  c.strokeStyle = "black";
-  // horizontal axis
-  c.moveTo(0, origin.y);
-  c.lineTo(canvas.width, origin.y);
-  // vertical axis
-  c.moveTo(origin.x, 0);
-  c.lineTo(origin.x, canvas.height);
-  c.stroke();
-
-  const left = Math.round((0 - origin.x) / scale.x);
-  const right = Math.round((canvas.width - origin.x) / scale.x);
-  const top = Math.round((0 - origin.y) / scale.y);
-  const bottom = Math.round((canvas.height - origin.y) / 2);
-
-  c.font = "12px Roboto Mono";
-  c.textAlign = "center";
-  for (let x = left; x < right; x++) {
-    c.fillText(`${x}`, origin.x + x * scale.x, origin.y + 20);
-  }
-
-  c.textAlign = "right";
-  c.textBaseline = "middle";
-  for (let y = top; y < bottom; y++) {
-    c.fillText(`${-y}`, origin.x - 4, origin.y + y * scale.y);
-  }
-}
-
-function registerOnMouseDrag(
-  el: HTMLElement,
-  ondrag: (p: Point) => void,
-  ondone: (p: Point) => void
-) {
-  let pressdown: Point | null = null;
-  el.addEventListener("mousedown", (e) => {
-    pressdown = {
-      x: e.pageX,
-      y: e.pageY,
+    this.origin = {
+      x: (unitaryOrigin.x * canvas.width) / 2 + canvas.width / 2,
+      y: (unitaryOrigin.y * canvas.height) / 2 + canvas.height / 2,
     };
-  });
-  document.addEventListener("mousemove", (e) => {
-    if (pressdown !== null) {
-      ondrag({
-        x: e.pageX - pressdown.x,
-        y: e.pageY - pressdown.y,
-      });
+
+    this.canvas.addEventListener("mousedown", (e) => {
+      this.pressdown = {
+        x: this.origin.x - e.pageX,
+        y: this.origin.y - e.pageY,
+      };
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      if (this.pressdown !== null) {
+        this.origin = {
+          x: e.pageX + this.pressdown.x,
+          y: e.pageY + this.pressdown.y,
+        };
+        this.render();
+      }
+    });
+
+    document.addEventListener("mouseup", (e) => {
+      if (this.pressdown !== null) {
+        this.pressdown = null;
+      }
+    });
+  }
+
+  render() {
+    if (this.func === null) return;
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this._renderAxis();
+
+    const left = Math.round((0 - this.origin.x) / this.scale.x);
+    const right = Math.round(
+      (this.canvas.width - this.origin.x) / this.scale.x
+    );
+
+    const radius = 4;
+    this.context.fillStyle = "#2c3e50";
+    for (let x = left; x < right; x++) {
+      this.context.beginPath();
+      const y = this.func(x);
+      this.context.ellipse(
+        x * this.scale.x + this.origin.x,
+        -y * this.scale.y + this.origin.y,
+        radius,
+        radius,
+        0,
+        0,
+        2 * Math.PI
+      );
+      this.context.fill();
     }
-  });
-  document.addEventListener("mouseup", (e) => {
-    if (pressdown !== null) {
-      ondone({
-        x: e.pageX - pressdown.x,
-        y: e.pageY - pressdown.y,
-      });
+  }
+
+  _renderAxis() {
+    this.context.strokeStyle = "black";
+    this.context.beginPath();
+
+    // horizontal axis
+    this.context.moveTo(0, this.origin.y);
+    this.context.lineTo(this.canvas.width, this.origin.y);
+
+    // vertical axis
+    this.context.moveTo(this.origin.x, 0);
+    this.context.lineTo(this.origin.x, this.canvas.height);
+
+    this.context.stroke();
+
+    const left = Math.round((0 - this.origin.x) / this.scale.x);
+    const right = Math.round(
+      (this.canvas.width - this.origin.x) / this.scale.x
+    );
+    const top = Math.round((0 - this.origin.y) / this.scale.y);
+    const bottom = Math.round((this.canvas.height - this.origin.y) / 2);
+
+    this.context.font = "12px Roboto Mono";
+    this.context.textAlign = "center";
+
+    for (let x = left; x < right; x++) {
+      this.context.fillText(
+        `${x}`,
+        this.origin.x + x * this.scale.x,
+        this.origin.y + 20
+      );
     }
-    pressdown = null;
-  });
+
+    this.context.textAlign = "right";
+    this.context.textBaseline = "middle";
+    for (let y = top; y < bottom; y++) {
+      this.context.fillText(
+        `${-y}`,
+        this.origin.x - 4,
+        this.origin.y + y * this.scale.y
+      );
+    }
+  }
 }
